3e2b4b4cda11424095858e16897426a8fcfbe57e
Vorix325 <fftpnhan2246@gmail.com>
2022-11-02 19:10:58 -0800

    Register Update

--- model/userInfo.php
+++ model/userInfo.php
@@ -42,5 +42,24 @@ class userInfo
     $statement->closeCursor();
     return $user; 
  }
-}
+ function addUser($username,$password,$fname,$lname,$email,$phone)
+ {
+     $db = Database::getDB();
+    $query = 'INSERT INTO user
+                (user_name, password, first_name, last_name, email, phone_number)
+               VALUES
+                 (:username, :password, :fname, :lname, :email, :phone)';
+            
+    $statement = $db->prepare($query);
+    $statement->bindValue(':username',$username);
+    $statement->bindValue(':password',$password);
+    $statement->bindValue(':fname',$fname);
+    $statement->bindValue(':lname',$lname);
+    $statement->bindValue(':email',$email);
+    $statement->bindValue(':phone',$phone);
+    $statement->execute();
+    $statement->closeCursor(); 
+ }
+         
+ }
 
--- user_info/index.php
+++ user_info/index.php
@@ -7,7 +7,7 @@ $userInfo = new userInfo();
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
+    $action = filter_input(INPUT_POST, 'action');
     if ($action == NULL) {
         $action = 'list_products';
     }
@@ -16,16 +16,42 @@ if ($action == NULL) {
 switch($action)
 {
     case 'login':
-        $username = filter_input(INPUT_GET, 'username');
-        $password = filter_input(INPUT_GET, 'password');
+        $username = filter_input(INPUT_POST, 'username');
+        $password = filter_input(IINPUT_POST, 'password');
         $check = $userInfo->checkLogin($username);
         if($check === $password)
         {
-            include();
+            include('../user_info/login.php');
+        }
+        else
+        {
+            include('../');
+        }
+    case 'register' :
+        $username = filter_input(INPUT_POST, 'username');
+        $password = filter_input(INPUT_POST, 'password');
+        $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+        $fname = filter_input(INPUT_POST, 'fname');
+        $lname = filter_input(INPUT_POST, 'lname');
+        $phone = filter_input(INPUT_POST, 'phone', FILTER_VALIDATE_INT);
+        if($email == FALSE)
+        {
+            $errorEmail = 'Please enter a valid email';
+            include('../user_info/login.php');
         }
         else
         {
             
         }
-    case 'register' :          
+        if($phone == FALSE)
+        {
+            $errorPhone = 'Please enter a valid phone';
+            include('../user_info/login.php');
+        }
+        if($username == null || $password == null || $email == null || $fname == null || $lname == null || $phone == null)
+        {
+            $errorEmpty = "Please enter fill out everything";
+            include('../user_info/login.php');
+        }
+        
 }
