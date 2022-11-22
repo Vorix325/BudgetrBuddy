f2ef21e6cd60686345a9aada12e0013881e19220
shaira-g <75226673+shaira-g@users.noreply.github.com>
2022-11-21 20:20:53 -0800

    css added

--- budget_page/budget_edit.php
+++ budget_page/budget_edit.php
@@ -1,24 +1,65 @@
-<?php include '../view/header.php'; ?>
-<main>
-    <h1>Add New Category</h1>
-    <form action="." method="post" id="add_product_form">
-        <input type="hidden" name="action" value="add_product">
-        <input type ="hidden" name="userId" Value="<?php echo $userId; ?>"><!-- comment -->
-        <input type ="hidden" name="month" Value="<?php echo $month; ?>"><!-- comment -->
-        <input type ="hidden" name="year" Value="<?php echo $year; ?>"><!-- comment -->
-        <label>Name:</label>
-        <input type="text" name="ca_name" value="<?php echo $name; ?>"/>
-        <br>
+<!DOCTYPE html>
 
-        <label>Limit:</label>
-        <input type="text" name="Limit" value='<?php echo $limit; ?>'/>
-        <br>
+<html lang="en">
+    <head>
+        <meta name="viewport"
+        content="width=device-width, initial-scale=1.0" />
+        <title>Edit Spending</title>
 
-        <label>&nbsp;</label>
-        <input type="submit" value="Add Product" />
-        <br>
-    </form>
-   
+        <!-- Google Font link -->
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
+            rel="stylesheet"
+        />
+        <!-- Stylesheet -->
+        <link rel="stylesheet" href="../bp-stylesheet.css" />
+    </head>
 
-</main>
-<?php include '../view/footer.php'; ?>
+    <body>
+        <!-- Header -->
+        <?php include '../view/header.php'; ?> 
+
+        <div class="wrapper">
+            <div class="container">
+                <div class="welcome">
+                    <h3>Need to adjust your spending costs? Edit your spending here.</h3>
+                </div>
+            </div>
+
+            <!-- Edit Spending -->
+            <div class="super-welcome">
+                <main>
+                    <h1>Edit Spending</h1><br>
+                    <form action="." method="post" id="add_product_form">
+                        <input type="hidden" name="action" value="updateSpending">
+                        <label>Select Budget Category:</label><br>
+                        <select name="category_id" class="select-category">
+                        <?php foreach ( $categories as $category ) : ?>
+                            <option value="<?php echo $category->getCaID(); ?>" <?php if( $category->getCaID() == $category_id ): ?> selected="selected" <?php endif; ?>>
+                                <?php echo $category->getCaName(); ?>
+                            </option>
+                        <?php endforeach; ?>
+                        </select>
+                        <br>
+                        <input type ="hidden" name='userId' value='<?php echo $userId; ?>'>
+                        <label>Name:</label>
+                        <input type="text" name="spendName" value="<?php echo $name; ?>" />
+                        <br>
+
+                        <label>Amount:</label>
+                        <input type="text" name="amount" value="<?php echo $amount; ?>" />
+                        <br>
+                        <label for="month">Select Month & Date of Spending:</label>
+                        <p class= "hide error" id="month-error">Date is invalid</p>
+                        <input type="date" class="month-date"  id="month-date" value="<?php echo $date; ?>">
+                        <br><br>
+                        <input type=button class="profile-submit" value="Update Spending" />
+                        <br>
+                    </form>
+                </main>
+            </div>
+        </div>
+        <!-- Footer -->
+        <?php include '../view/footer.php'; ?>
+    </body>
+</html>
